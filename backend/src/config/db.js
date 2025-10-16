9136555972577f6147bf1a4892b87a7cc3f068df
Gurram Yagnasree Chowdary <gurramyagna2004@gmail.com>
2025-10-16 10:08:58 +0530

    Comment out dotenv config in db.js
    Comment out dotenv configuration for environment variables.

--- backend/src/config/db.js
+++ backend/src/config/db.js
@@ -1,4 +1,4 @@
-require('dotenv').config();
+//require('dotenv').config();
 const { Pool } = require('pg');
 console.log("Attempting to create database pool...")
 const pool = new Pool({ connectionString: process.env.DATABASE_URL });
